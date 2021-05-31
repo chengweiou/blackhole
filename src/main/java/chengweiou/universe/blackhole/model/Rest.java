4370ec23e7ee7d274ffb4248cee6b3c1a1b63da9
chengweiou <chengwei.ou.dora@gmail.com>
2021-05-30 19:41:25 -0700

    fix rest localdatetime adapter

--- src/main/java/chengweiou/universe/blackhole/model/Rest.java
+++ src/main/java/chengweiou/universe/blackhole/model/Rest.java
@@ -1,14 +1,18 @@
 package chengweiou.universe.blackhole.model;
 
-import com.google.gson.*;
-import com.google.gson.reflect.TypeToken;
-
 import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
+import java.time.ZoneId;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializer;
+import com.google.gson.reflect.TypeToken;
 
 public class Rest<T> implements Serializable {
     private RestCode code;
@@ -62,9 +66,9 @@ public class Rest<T> implements Serializable {
     private static Gson createGson(Class c) {
         return new GsonBuilder()
                 .registerTypeHierarchyAdapter(RestCode.class, (JsonDeserializer<?>) (json, typeOfT, context) -> Enum.valueOf(c, json.getAsString()))
-                .registerTypeAdapter(LocalDate.class, (JsonDeserializer) (json, typeOfT, context) -> ZonedDateTime.parse(json.getAsJsonPrimitive().getAsString()).toLocalDate())
+                .registerTypeAdapter(LocalDate.class, (JsonDeserializer) (json, typeOfT, context) -> LocalDate.parse(json.getAsJsonPrimitive().getAsString()))
                 .registerTypeAdapter(LocalDate.class, (JsonSerializer) (v, typeOfT, context) -> new JsonPrimitive(v.toString()))
-                .registerTypeAdapter(LocalDateTime.class, (JsonDeserializer) (json, typeOfT, context) -> ZonedDateTime.parse(json.getAsJsonPrimitive().getAsString()).toLocalDateTime())
+                .registerTypeAdapter(LocalDateTime.class, (JsonDeserializer) (json, typeOfT, context) -> LocalDateTime.parse(json.getAsJsonPrimitive().getAsString()).atZone(ZoneId.systemDefault()).toLocalDateTime())
                 .registerTypeAdapter(LocalDateTime.class, (JsonSerializer) (v, typeOfT, context) -> new JsonPrimitive(v.toString()))
                 .create();
     }
